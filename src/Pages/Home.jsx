9c8db5340d4a6a9c9a389bf2d4871c1403f93826
IshaPancholi22 <isha.empbweb@gmail.com>
2024-09-19 10:18:56 +0530

    side wall design

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,12 @@ import artImg1 from "../assets/img/art-img/1_PrajjwalChoudhury.jpg";
 import artImg2 from "../assets/img/art-img/2_UdayMondal.jpg";
 import artImg3 from "../assets/img/art-img/3_BalajiPonna.jpg";
 import artImg4 from "../assets/img/art-img/4_NimeshPatel.jpg";
+import artImg5 from "../assets/img/art-img/25_uaymondalBig.jpg";
+import artImg6 from "../assets/img/art-img/26_NikunjPatel.jpg";
+import artImg7 from "../assets/img/art-img/27_HeeralTrivedi1.jpg";
+import artImg8 from "../assets/img/art-img/28_Heeral Trivedi2.jpg";
+import artImg9 from "../assets/img/art-img/29_devdusawar.jpg";
+import artImg10 from "../assets/img/art-img/30_tvenkanna.jpg";
 
 import artImgBig from "../assets/img/art-img/25_uaymondalBig.jpg";
 
@@ -29,6 +35,20 @@ export const Home = () => {
         setLeftWallData(temp)
         setRightWallData(temp3)
     }
+    const sideWall = [
+        {
+            url: require('../assets/img/art-img/25_uaymondalBig.jpg')
+        },
+        {
+            url: require('../assets/img/art-img/26_NikunjPatel.jpg')
+        },
+        {
+            url: require('../assets/img/art-img/25_uaymondalBig.jpg')
+        },
+        {
+            url: require('../assets/img/art-img/25_uaymondalBig.jpg')
+        }
+    ]
     return (
         <React.Fragment>
             <section className="main-img">
@@ -93,7 +113,30 @@ export const Home = () => {
 
                             </div>
                         </div>
-                        <div className="col-lg-3 col-12"></div>
+                        <div className="col-lg-1 col-12">
+                <div className="row pers-two">
+                    {sideWall.map((item, index)=> (
+                        <div className="col-lg-12" key={index}>
+                        <div className="side-img-sm">
+                            <Link to="/" className="img-link">
+                            <img src={item.url} className="w-100" alt=""/>
+                            </Link>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            </div>
+            <div className="col-lg-2 col-12">
+                <div className="row pers-three">
+                    <div className="col-lg-12">
+                        <div className="wall-img">
+                            <a href="inner-page.html" className="img-link">
+                                <img src={artImg10} className="w-100" alt=""/>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
                     </div>
                 </div>
             </section>
